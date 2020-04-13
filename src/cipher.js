5cebd2550faa7b32ed1b305b83160066fa25fe4f
jebarros <jessicas.dbarros@gmail.com>
2020-04-13 10:26:58 -0300

    Projeto

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,44 @@
 const cipher = {
-  // ...
-};
+  encode: function (deslocamento, mensagem) {
 
+
+    if (typeof deslocamento !== 'number') {
+      throw TypeError("Não confere")
+    }
+
+    if (typeof mensagem !== 'string') {
+      throw TypeError ("Não confere")
+    }
+
+
+    let xuxu = "";
+    let i;
+
+    for (i = 0; i < mensagem.length; i++) {
+      let letra = mensagem.charCodeAt(i);
+      let ASC = ((letra - 65 + deslocamento) % 26) + 65;
+
+      xuxu += String.fromCharCode(ASC);
+    }
+
+    return xuxu
+  },
+
+
+  decode: function (deslocamento, mensagem) {
+    let jaca = "";
+    let i
+
+    for (i = 0; i < mensagem.length; i++) {
+      let letra = mensagem.charCodeAt(i);
+      let ASC = ((letra - 90 - deslocamento) % 26) + 90;
+      jaca += String.fromCharCode(ASC)
+    }
+
+    return jaca
+
+  }
+
+
+};
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+const botaoCifra = document.getElementById("cifrar")
+const botaoDecifra = document.getElementById("decifrar")
+
+botaoCifra.addEventListener("click", function () {
+  let mensagem = document.getElementById("mensagem").value;
+  let deslocamento = document.getElementById("deslocamento").value;
+  deslocamento = parseInt(deslocamento);
+  mensagem = mensagem.toUpperCase();
+  document.getElementById("result").value = cipher.encode(deslocamento, mensagem);
+
+
+});
+
+botaoDecifra.addEventListener("click", function() {
+  let mensagem = document.getElementById("mensagem").value;
+  let deslocamento = document.getElementById("deslocamento").value;
+  deslocamento = parseInt(deslocamento);
+  mensagem = mensagem.toUpperCase();
+  document.getElementById("result").value = cipher.decode(deslocamento, mensagem);
+});
